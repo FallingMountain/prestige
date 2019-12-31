7491b0dfa388d9518683e6c2752417f9f55e157a
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-12-30 18:01:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 var data = {
 	coins: 0,
-	prestiges: [0,0,0,0,0,0,0,0,0,0]
+	prestiges: [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
 };
 
 function getGain() {
-	var gain = 1;
-	data.prestiges.forEach(function (el) {
-		gain *= 1+el;
-	})
-	return gain;
+	var gain = 1
+	for (var i = 0, i<10, i++) {
+	gain *= Math.floor(Math.pow((1+data.prestiges[i][0]), (1+data.prestiges[i][1])));
+		
+	}
 }
 
 function getRequirement(id) {
 	if (id === 0) {
-		return Math.floor(Math.pow(1.5,data.prestiges[0])*10);
+		return Math.floor(Math.pow(1.5,data.prestiges[0][0])*10);
 	} else {
-		return Math.pow(id+1,data.prestiges[id]+1)
+		return Math.pow(id+1,data.prestiges[id][0]+1)
 	}
 }
 
@@ -23,7 +23,7 @@ function canActivatePrestige(id) {
 	if (id===0) {
 		return (data.coins >= getRequirement(0));
 	} else {
-		return (data.prestiges[id-1] >= getRequirement(id));
+		return (data.prestiges[id-1][0] >= getRequirement(id));
 	}
 }
 
@@ -31,36 +31,52 @@ function activatePrestige(id) {
 	if (canActivatePrestige(id)) {
 			data.coins = 0;
 			for (var i = 0; i < id; i++) {
-				data.prestiges[i] = 0;
+				data.prestiges[i][0] = 0;
 			}
-			data.prestiges[id]++;
+			data.prestiges[id][0]++;
 	}
 	draw();
 }
 
 function update() {
-	data.coins += getGain();
-	localStorage.SHITPOST = JSON.stringify(data);
+	data.coins += getGain()/30;
+	localStorage.whydididothis = JSON.stringify(data);
 }
-
+function boostMac(tier) {
+	let boost = Math.log10(data.prestiges[tier][0]);
+	if (boost > data.prestiges[tier][1]) return boost
+	else return data.prestiges[tier][1]
+}
 function draw() {
 	document.getElementById("coins").innerHTML = data.coins;
-	document.getElementById("gain").innerHTML = getGain();
-	data.prestiges.forEach(function (el, i) {
+	document.getElementById("gain").innerHTML = getGain()/30;
+	//To hide the boost machine
+	var boostT1 = document.getElementsByClassName("boost");
+	if(data.prestiges[2][0] > 0) {
+	for(var i = 0; i < boostT1.length; i++) {
+	boostT1[i].style.display = "";
+	}
+	}else {
+	for(var i = 0; i < boostT1.length; i++) {
+	boostT1[i].style.display = "none";
+	}}
+	
+	for(var i = 0; i < 10; i++) {
 		document.getElementById("tier"+(i+1)+"cost").innerHTML = getRequirement(i);
-		document.getElementById("tier"+(i+1)+"a").innerHTML = el;
-		document.getElementById("tier"+(i+1)+"mul").innerHTML = "x"+(el+1);
+		document.getElementById("tier"+(i+1)+"a").innerHTML = data.prestiges[i][0];
+		document.getElementById("tier"+(i+1)+"mul").innerHTML = "x"+(data.prestiges[i][0]+1);
 		if (canActivatePrestige(i)) {
 			document.getElementById("tier"+(i+1)+"btn").disabled = false;
 		} else {
 			document.getElementById("tier"+(i+1)+"btn").disabled = true;
 		}
-	})
+	}
+	document.getElementById("tier1b") = "^"+Math.floor(data.prestiges[0][1]);
 }
 
 window.addEventListener("load",function () {
-	if (localStorage.SHITPOST) {
-		data = JSON.parse(localStorage.SHITPOST)
+	if (localStorage.whydididothis) {
+		data = JSON.parse(localStorage.whydididothis)
 	}
 	draw();
 	for (var i = 0; i < 10; i++) {
@@ -76,6 +92,6 @@ window.addEventListener("load",function () {
 	setInterval(function () {
 		update();
 		draw();
-	}, 1000);
+	}, 33);
 	console.log("interval loaded")
 })
